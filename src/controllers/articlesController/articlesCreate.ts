770897c5e2da6b9d913b65016855d910d86faaa4
Ronan Pardo Soares <ronan.soares@gmail.com>
2022-11-07 10:02:56 -0300

    Refactor articleCreate

--- src/controllers/articlesController/articlesCreate.ts
+++ src/controllers/articlesController/articlesCreate.ts
@@ -1,6 +1,6 @@
 import { Tag } from "@prisma/client";
 import { NextFunction, Response } from "express";
-import { Request as JWTRequest, UnauthorizedError } from "express-jwt";
+import { Request as JWTRequest } from "express-jwt";
 import articleCreatePrisma from "../../utils/db/articleCreatePrisma";
 import tagsCreatePrisma from "../../utils/db/tagsCreatePrisma";
 
@@ -18,29 +18,13 @@ export default async function articlesCreate(
 ) {
   const { title, description, body, tagList }: Article = req.body.article;
   const user = req.auth?.user;
-  if (!user) {
-    next(
-      new UnauthorizedError(
-        "credentials_required",
-        new Error("user not authenticated")
-      )
-    );
-  }
-  if (!user.username) {
-    next(
-      new UnauthorizedError(
-        "credentials_bad_scheme",
-        new Error("username not in the token")
-      )
-    );
-  }
 
   let tags: Tag[] = [];
   if (tagList && tagList.length > 0) {
     try {
       tags = await tagsCreatePrisma(tagList);
     } catch (error) {
-      next(error);
+      return next(error);
     }
   }
   try {
@@ -53,6 +37,6 @@ export default async function articlesCreate(
     const response = { ...article, tagList };
     return res.status(201).json(response);
   } catch (error) {
-    next(error);
+    return next(error);
   }
 }
