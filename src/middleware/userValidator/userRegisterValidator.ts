6b1f1191ecfc1200eed80bbbe95dc7caec981f11
Ronan Pardo Soares <ronan.soares@gmail.com>
2022-11-09 08:47:03 -0300

    Refactor userRegisterValidator

--- src/middleware/userValidator/userRegisterValidator.ts
+++ src/middleware/userValidator/userRegisterValidator.ts
@@ -1,5 +1,4 @@
 import { NextFunction, Request, Response } from "express";
-import logger from "../../utils/logger";
 
 interface ValidationError {
   body?: Array<string>;
@@ -18,37 +17,38 @@ export default async function userRegisterValidator(
   res: Response,
   next: NextFunction
 ) {
-  logger.debug("Starting - userRegisterValidator");
   const errors: ValidationError = {};
   errors.body = [];
   if (!req.body) {
     errors.body.push("can't be empty");
-  } else {
-    const { user } = req.body;
-    if (!user) {
-      errors.body.push("user object must be defined");
-    } else {
-      const { password, email, username } = user;
-      if (!password) {
-        errors.body.push("password property in user can't be empty");
-      } else if (typeof password != "string") {
-        errors.body.push("password property in user must be a string");
-      }
-      if (!email) {
-        errors.body.push("email property in user can't be empty");
-      } else if (typeof email != "string") {
-        errors.body.push("email property in user must be a string");
-      }
-      if (!username) {
-        errors.body.push("username property in user can't be empty");
-      } else if (typeof username != "string") {
-        errors.body.push("username property in user must be a string");
-      }
-    }
+    return res.status(422).json({ errors });
   }
+
+  const { user } = req.body;
+  if (!user) {
+    errors.body.push("user object must be defined");
+    return res.status(422).json({ errors });
+  }
+
+  const { password, email, username } = user;
+  if (!password) {
+    errors.body.push("password property in user can't be empty");
+  } else if (typeof password != "string") {
+    errors.body.push("password property in user must be a string");
+  }
+
+  if (!email) {
+    errors.body.push("email property in user can't be empty");
+  } else if (typeof email != "string") {
+    errors.body.push("email property in user must be a string");
+  }
+
+  if (!username) {
+    errors.body.push("username property in user can't be empty");
+  } else if (typeof username != "string") {
+    errors.body.push("username property in user must be a string");
+  }
+
   if (errors.body.length) return res.status(422).json({ errors });
-  else {
-    logger.debug("user validated correctly");
-    next();
-  }
+  next();
 }
