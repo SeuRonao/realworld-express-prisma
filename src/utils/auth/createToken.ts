809d84899e0f29dc69e8f5cca48209802ad6d615
Ronan Pardo Soares <ronan.soares@gmail.com>
2022-11-09 09:09:58 -0300

    Refactor createToken

--- src/utils/auth/createToken.ts
+++ src/utils/auth/createToken.ts
@@ -4,9 +4,8 @@ import jwt from "jsonwebtoken";
 dotenv.config();
 
 export default function createToken(information: string) {
-  if (!process.env.JWT_SECRET) {
+  if (!process.env.JWT_SECRET)
     throw new Error("JWT_SECRET missing in environment.");
-  }
   const token = jwt.sign(information, process.env.JWT_SECRET);
   return token;
 }
