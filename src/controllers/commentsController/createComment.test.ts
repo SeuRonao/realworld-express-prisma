9998b05a5d172dd6ed8c6f7100a1c26f523722ac
Ronan Pardo Soares <ronan.soares@gmail.com>
2022-11-14 13:26:52 -0300

    Adding placeholder for testing

---
+++ src/controllers/commentsController/createComment.test.ts
@@ -0,0 +1,9 @@
+function notImplementedYet() {
+  throw new Error("Not implemented yet");
+}
+
+describe("Create Comment Controller", function () {
+  test("Success path", notImplementedYet);
+  test("User does not exist path", notImplementedYet);
+  test("Comment cannot be created", notImplementedYet);
+});
---
+++ src/middleware/commentsValidator/commentsCreateValidator.test.ts
@@ -0,0 +1,13 @@
+function notImplementedYet() {
+  throw new Error("Not implemented yet");
+}
+
+describe("Test commentsCreateValidator", function () {
+  test("Request formatted correctly", notImplementedYet);
+  test("Request does not have comment property in body", notImplementedYet);
+  test("Comment inside body of request is not an object", notImplementedYet);
+  test(
+    "There is no string property inside comment on the body",
+    notImplementedYet
+  );
+});
