93017841c9585be1c6ff063345dd97d5effc0f45
Ronan Pardo Soares <ronan.soares@gmail.com>
2022-11-14 13:46:16 -0300

    WIP: testing

--- src/middleware/commentsValidator/commentsCreateValidator.test.ts
+++ src/middleware/commentsValidator/commentsCreateValidator.test.ts
@@ -1,9 +1,23 @@
+import { Response } from "express";
+import { Request } from "express-jwt";
+import commentCreateValidator from "./commentCreateValidator";
+
 function notImplementedYet() {
   throw new Error("Not implemented yet");
 }
 
 describe("Test commentsCreateValidator", function () {
-  test("Request formatted correctly", notImplementedYet);
+  const mockRes = {
+    status: jest.fn(() => mockRes),
+    json: jest.fn(() => mockRes),
+  } as unknown as Response;
+  const next = jest.fn();
+  test("Request formatted correctly", function () {
+    const mockReq = {
+      body: { comment: { body: "Test comment body" } },
+    } as Request;
+    commentCreateValidator(mockReq, mockRes as unknown as Response, next);
+  });
   test("Request does not have comment property in body", notImplementedYet);
   test("Comment inside body of request is not an object", notImplementedYet);
   test(
