b962651bc09dfd302cc62ec7fe004c84d3ace687
Ronan Pardo Soares <ronan.soares@gmail.com>
2022-11-16 10:33:29 -0300

    Add testing to comments creation validator

--- src/middleware/commentsValidator/commentsCreateValidator.test.ts
+++ src/middleware/commentsValidator/commentsCreateValidator.test.ts
@@ -2,26 +2,39 @@ import { Response } from "express";
 import { Request } from "express-jwt";
 import commentCreateValidator from "./commentCreateValidator";
 
-function notImplementedYet() {
-  throw new Error("Not implemented yet");
-}
-
 describe("Test commentsCreateValidator", function () {
   const mockRes = {
     status: jest.fn(() => mockRes),
     json: jest.fn(() => mockRes),
   } as unknown as Response;
   const next = jest.fn();
-  test("Request formatted correctly", function () {
+
+  test("Request formatted correctly", async function () {
     const mockReq = {
       body: { comment: { body: "Test comment body" } },
     } as Request;
-    commentCreateValidator(mockReq, mockRes as unknown as Response, next);
+    commentCreateValidator(mockReq, mockRes, next);
+    expect(next).toHaveBeenCalled();
   });
-  test("Request does not have comment property in body", notImplementedYet);
-  test("Comment inside body of request is not an object", notImplementedYet);
-  test(
-    "There is no string property inside comment on the body",
-    notImplementedYet
-  );
+  test("Request does not have comment property in body", async function () {
+    const mockReq = {
+      body: {},
+    } as Request;
+    commentCreateValidator(mockReq, mockRes, next);
+    expect(mockRes.status).toBeCalledWith(400);
+  });
+  test("Comment inside body of request is not an object", async function () {
+    const mockReq = {
+      body: { comment: "This should be an object instead" },
+    } as Request;
+    commentCreateValidator(mockReq, mockRes, next);
+    expect(mockRes.status).toBeCalledWith(400);
+  });
+  test("There is no string property inside comment on the body", async function () {
+    const mockReq = {
+      body: { comment: {} },
+    } as Request;
+    commentCreateValidator(mockReq, mockRes, next);
+    expect(mockRes.status).toBeCalledWith(400);
+  });
 });
